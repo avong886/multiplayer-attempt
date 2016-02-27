1948555de79ba00c48d6c53e1f58dd6fc6f77c01
avong886 <alexvong8@gmail.com>
2016-02-26 22:39:48 -0800

    Create Add_player.php

---
+++ Add_player.php
@@ -0,0 +1,25 @@
+
+<?php
+$servername = "localhost";
+$username = "cryszwjw_g_user";
+$password = "vWNxak!auom3";
+$dbname = "cryszwjw_g_stats";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO current_players (player_name, playerlocation)
+VALUES ('TheFirstPlayer', '(0,1,2')";
+
+if ($conn->query($sql) === TRUE) {
+echo "New record created successfully";
+} else {
+echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
